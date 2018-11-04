271698d9fd8d6a73a277956256e76977df49630f
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-04 16:46:35 +0900

    linked Dispatch and GetState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dispatch from './components/Dispatch';
+import GetState from './components/GetState';
 
 const App = () => (
   <div>
     Redux test
+    <Dispatch />
+    <GetState />
   </div>
 );
 
