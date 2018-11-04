66090c3a146042854f26f6760e77f56ef1407ca3
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-04 16:46:59 +0900

    connected redux state and app

---
+++ src/components/Dispatch.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import addInfo from '../redux/actions';
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addInfo: info => dispatch(addInfo(info)),
+//   };
+// };
+
+const Dispatch = () => {
+  return (
+    <div>
+      Dispatch
+    </div>
+  );
+};
+
+// class Connectedbutton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this
+//   }
+// }
+
+// const Dispatch = connect(null, mapDispatchToProps)(Connectedbutton);
+
+export default Dispatch;
