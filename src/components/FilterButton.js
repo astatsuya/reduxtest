6d6d0cec113b4a513cf8ccf16dc82c2a5b6e1b12
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-06 14:13:37 +0900

    revised to const

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,42 +1,36 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { filter } from '../redux/reducer';
 
-// const FilterButton = (props) => {
-//   const tes = (props) => {
-//     alert(props)
+const FilterButton = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>FIlter</button>
+    </div>
+  );
+};
+
+// class FilterButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.clickToParent = this.clickToParent.bind(this);
 //   }
 //
-//   return (
-//     <div>
-//       <button onClick={tes(props.onClick)}>Filter</button>
-//       <button>{props.onClick}</button>
-//     </div>
-//   );
-// };
-
-class FilterButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.clickToParent = this.clickToParent.bind(this);
-  }
-
-  clickToParent() {
-    this.props.onClick();
-  }
-
-  render() {
-    return (
-      <div>
-        <button >Filter</button>
-        <button onClick={this.clickToParent}>
-          {this.props.filterContents}
-        </button>
-      </div>
-    )
-  }
-}
+//   clickToParent() {
+//     this.props.onClick();
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <button >Filter</button>
+//         <button onClick={this.clickToParent}>
+//           {this.props.filterContents}
+//         </button>
+//       </div>
+//     )
+//   }
+// }
 
 
 export default FilterButton;
